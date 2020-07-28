a41f854546d03716298d87602f0a230d55416bbe
olegmalyarenko <oi.malyarenko@gmail.com>
2020-07-28 22:49:54 +0300

    feat: third task html elements

--- third-task.js
+++ third-task.js
@@ -2,12 +2,12 @@ const firstData = 'https://swapi.dev/api/people/';
 const secondData = 'https://swapi.dev/api/starships/';
 
 const getXhrData = (data) => {    
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', data, true);
+        xhr.open('GET', data);
         xhr.onload = () => {
           if (xhr.status == 200) {
-            resolve(console.log(xhr.responseText));
+            resolve(xhr.response);
             
           } else {
             reject(xhr.status);
@@ -18,5 +18,12 @@ const getXhrData = (data) => {
       })
 }
 
-getXhrData(firstData);
-getXhrData(secondData);
+getXhrData(firstData).then(data1 => {
+    getXhrData(secondData).then(data2 => {
+        console.log(data1, data2);
+        console.log('Все данные получены');
+        document.getElementById('spinner').style.display = 'none'; 
+        document.getElementById('complited-text').style.display = 'block'; 
+
+     })
+  }).catch(err => console.log(err.message));
