01994d06abb17c5606c546ee3024e066e6cfca90
olegmalyarenko <oi.malyarenko@gmail.com>
2020-07-31 01:37:58 +0300

    feat: update prodject structure

--- forth-task/fourth-task-two/domain-two.js
+++ forth-task/fourth-task-two/domain-two.js
@@ -4,12 +4,17 @@ document.domain = '127.0.0.1';
 window.onmessage = function(e) {
     
     const payload = JSON.parse(e.data);
+
+    const crossMessage = (val) => {
+        alert(val);
+    }
     
     switch(payload.method) {
         
         case 'set':
             localStorage.setItem(payload.key, JSON.stringify(payload.data));
             console.log('written');
+            crossMessage('set callback');
             break;
 
         case 'get':
@@ -17,11 +22,17 @@ window.onmessage = function(e) {
             var data = localStorage.getItem(payload.key);
             console.log(data);
             parent.postMessage(data, "*");
+            crossMessage('get callback');
             break;
 
         case 'remove':
             localStorage.removeItem(payload.key);
             console.log('removed');
+            crossMessage('remove callback');
             break;
     }
+    
+    
+   
 };
+
