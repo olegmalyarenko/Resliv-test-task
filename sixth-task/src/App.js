c6f77bdfb2159a8b5d57827a04bd7a57a5bd4e8e
olegmalyarenko <oi.malyarenko@gmail.com>
2020-08-01 20:20:16 +0300

    feat: get api data

--- sixth-task/src/App.js
+++ sixth-task/src/App.js
@@ -3,7 +3,26 @@ import MainPage from './components/main-page';
 import EmployeesPage from './components/employees';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/navigation';
+const MyContext = React.createContext(null);
 export default class App extends Component {
+  state = {
+    itemList: [],
+    status: true,
+
+};
+componentDidMount(){
+  if (this.state.itemList.length === 0) {
+  fetch(`https://reqres.in/api/users?per_page=12`)
+  .then(res=> res.json())
+  .then( data => {
+    this.setState({
+      itemList: data.data
+    })
+  }).then(console.log(this.state.itemList))
+}
+
+
+}
   render() {
       return (
     <div className="App">
@@ -11,8 +30,14 @@ export default class App extends Component {
        <Router>
            <Navigation/>
            <Switch>
+           <MyContext.Provider
+              value={{
+                state: this.state,
+                
+              }}>
              <Route path='/' component={MainPage} exact />
              <Route path='/employees' component={EmployeesPage} />
+             </MyContext.Provider>
            </Switch> 
 
            
